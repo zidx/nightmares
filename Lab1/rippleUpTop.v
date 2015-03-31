3b8470c2b4ef4156a9db52c8008da1d8082b5cb9
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2015-03-31 14:53:53 -0700

    dfghjklkjhgfd

--- Lab1/rippleUpTop.v
+++ Lab1/rippleUpTop.v
@@ -6,6 +6,24 @@ module testBench;
 
 endmodule
 
-module Tester();
-
+module Tester(rst, clk, out);
+	input clk, rst;
+	output reg [3:0] out;
+	
+	paramater simDelay = 20;
+	
+	// Generate periodic clock signal
+	always begin
+		#simDelay clk = !clk;
+	end
+	
+	initial  // Response
+	begin
+		$display("\t\t out \t rst \t clk \t Time ");
+		$monitor("\t\t %b \t %b \t %b", out, rst, clk, $time );
+	end
+	
+	initial  // Stimulus
+	begin
+		
 endmodule
