b289ae9d8024ad297641e9d2dcdea731327c18b5
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2015-03-31 15:55:55 -0700

    Fixed Riple

--- Lab1/rippleUp.v
+++ Lab1/rippleUp.v
@@ -9,7 +9,7 @@ module RippleUp(rst, clk, out);
 	wire qBar1, qBar2, qBar3, qBar4;
 	
 	DFlipFlop flip1(out[0], qBar1, qBar1, clk, rst);
-	DFlipFlop flip2(out[1], qBar2, qBar2, out[0], rst);
-	DFlipFlop flip3(out[2], qBar3, qBar3, out[1], rst);
-	DFlipFlop flip4(out[3], qBar4, qBar4, out[2], rst);
+	DFlipFlop flip2(out[1], qBar2, qBar2, qBar1, rst);
+	DFlipFlop flip3(out[2], qBar3, qBar3, qBar2, rst);
+	DFlipFlop flip4(out[3], qBar4, qBar4, qBar3, rst);
 endmodule
