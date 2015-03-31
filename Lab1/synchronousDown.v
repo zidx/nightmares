a4e03f31ba7c15c113ed4067cfe72c1d625e42e4
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2015-03-31 15:33:27 -0700

    added synchronous down counter

---
+++ Lab1/synchronousDown.v
@@ -0,0 +1,22 @@
+// Synchronous Down counter
+
+`include "flipflop.v"
+
+module SynchronousDown(rst, clk, out);
+	input clk, rst;
+	output [3:0] out;
+	
+	wire d0, d1, d2, d3;
+	wire qBar0, qBar1, qBar2, qBar3;
+	
+	DFlipFlop flip0(out[0], qBar0, d0, clk, rst);
+	DFlipFlop flip1(out[1], qBar1, d1, clk, rst);
+	DFlipFlop flip2(out[2], qBar2, d2, clk, rst);
+	DFlipFlop flip3(out[3], qBar3, d3, clk, rst);
+	
+	// Assignments for flip flop entrances
+	assign d0 = ~out[0];
+	assign d1 = ~(out[0] ^ out[1]);
+	assign d2 = (out[2] & (out[1] | out[0])) | ~(out[2] | out[1] | out[0]);
+	assign d3 = (out[3] & (out[2] | out[1] | out[0])) | ~(out[3] | out[2] | out[1] | out[0]);
+endmodule
