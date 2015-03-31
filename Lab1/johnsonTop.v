e3047a0d6bbcc85d36bd0429780ab27f76c33efa
Krista Holden <kl.holden@yahoo.com>
2015-03-31 16:01:20 -0700

    Da New Top

--- Lab1/johnsonTop.v
+++ Lab1/johnsonTop.v
@@ -1,5 +1,6 @@
 // johnsonTop.v
 `include "johnson.v"
+`include "tester.v"
 
 module testBench;
   // connect the two modules
@@ -18,52 +19,4 @@ module testBench;
     $dumpfile("johnson.vcd");
     $dumpvars(1,myJohnson);
     end
-endmodule 
-
-// johnsonTop.v cont.
-module Tester (rst, clk, out);
-	input [3:0] out;
-	output reg clk, rst;
-
-  parameter stimDelay = 20;
-
-  initial // Response
-  begin
-    $display("\t\t out \t rst \t clk \t Time ");
-		$monitor("\t\t %b \t %b \t %b", out, rst, clk, $time );
-  end
-
-  initial // Stimulus
-  begin
-    clk = 0; rst = 0;
-    #stimDelay clk = 1; rst = 1;
-    #stimDelay clk = 0; 
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; 
-    #stimDelay clk = 0;
-    #stimDelay clk = 1;  
-    #stimDelay clk = 0; 
-    #stimDelay clk = 1;
-    #stimDelay clk = 0;
-    #stimDelay clk = 1; rst = 0;
-    #stimDelay clk = 0;
-	#stimDelay clk = 1; rst = 1;
-    #stimDelay clk = 0;
-	#stimDelay clk = 1;
-    #stimDelay clk = 0;
-
-    #(2*stimDelay); // needed to see END of simulation
-    $finish; // finish simulation
-  end
 endmodule 
