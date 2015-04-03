6c252bac175fb6c6efecf8af23a96015cd2dd6b5
unknown <codyohl@EE-137PC03.ee.washington.edu>
2015-04-02 19:32:50 -0700

    Cody is a fucking loser who has no hope at passing this class

---
+++ Lab1/DownCounterSchematic/output_files/ClockDivider.sv
@@ -0,0 +1,15 @@
+module ClockDivider (clock, out);
+	 input clock;
+	 output out;
+	 reg out;
+	 reg [31:0] divided_clocks;
+
+	 initial
+	 divided_clocks = 0;
+
+	 always @(posedge clock)
+	 divided_clocks = divided_clocks + 1;
+	 
+	 parameter whichClock = 20;
+	 assign out = divided_clocks[whichClock];
+endmodule 
