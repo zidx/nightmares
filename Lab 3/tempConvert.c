0051cf3b7e8c87088777f51a5ccdd16aceb127d2
Krista Holden <klholden@uw.edu>
2015-04-28 16:09:26 -0700

    Create tempConvert.c

---
+++ Lab 3/tempConvert.c
@@ -0,0 +1,95 @@
+//  preprocessor directive to support printing to the display
+#include <stdio.h>
+
+void welcome();
+void convertC2K(double *value);
+void convertK2F(double *value);
+void convertF2C(double *value);
+
+bool checkNotBelowAbs0(int type, double *value);
+int getChoice();
+
+int main(int argc, char *args) {
+	// Prints out a welcome
+	welcome();
+
+	// Initializes values
+	double temp = 0.0;	
+	int convertFrom;
+	int convertTo;
+
+	// Get the temp input value
+	printf("Please enter your tempature value: ");
+	scanf("%lf", &temp);
+
+	// Gets the given value units
+	printf("Enter Given Tempature Units\n");
+	convertFrom = getChoice();
+
+	// Checks if the input is valid
+	if(!checkNotBelowAbs0(convertFrom, temp) {
+		printf("Your value is below Absolute Zero and therefore unrealistic.");
+	} else {
+		// Get the desired units
+		printf("Enter Desired Units\n");
+		convertTo = getChoice();
+
+		// Loop and keep converting until the value is converted as desired
+		while(convertFrom != convertTo) {
+			if(convertFrom == 1) convertK2F(temp);
+			else if(convertFrom == 2) convertF2C(temp);
+			else convertC2K(temp);
+			convertFrom = (convertFrom % 3) + 1;
+		}
+		printf("\nYour value converted is " + temp);
+	}	
+}
+
+void welcome() {
+	printf("Welcome to the tempature converter!\n");
+	printf("We cover all of your tempature converting needs."
+		+ " Have a value in Kelvin, Celsius, or Fahrenheit, but "
+		+ "want your value in another unit? We'll convert it!\n");
+	printf("\nOf course if your input isn't valid (i.e below"
+		+ " Absolute Zero) we can't help you!\n\n");
+}
+
+void convertC2K(double *value) {
+	double old = *value;
+	*value = old + 273.15;
+}
+
+void convertK2F(double *value) {
+	double old = *value;
+	*value = (old - 273.15) * 1.8000 + 32.00;
+}
+
+void convertF2C(double *value) {
+	double old = *value;
+	*value = (old - 32.0) * (5.0 / 9.0);
+}
+
+bool checkNotBelowAbs0(int type, double *value) {
+	if(type == 1) return *value >= 0.0;
+	else if(type == 2) return *value >= -273.15;
+	else return *value >= -459.67;
+}
+
+int getChoice() {
+	int choice = -1;
+	while (0 > choice || choice > 4)
+		printf("Please enter the # representing your choice:\n");
+		printf("(1) Kelvin\n");
+		printf("(2) Fahrenheit\n");
+		printf("(3) Celsius\n");
+		printf("Choice: ");	
+
+		scanf("%u", &choice);
+
+		if(0 > choice || choice > 4) {
+			printf("\nYour choice was not one of the integers 1, 2, or 3.\n");
+			printf("Please try again\n\n");
+		}
+	}
+	return choice;
+}
