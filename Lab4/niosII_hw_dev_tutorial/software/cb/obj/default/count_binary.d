3d325233a26bda052466a177b2f3479dc28d71aa
unknown <klholden@EE-137PC02.ee.washington.edu>
2015-05-26 15:18:49 -0700

    fssssssd

---
+++ Lab4/niosII_hw_dev_tutorial/software/cb/obj/default/count_binary.d
@@ -0,0 +1,23 @@
+obj/default/count_binary.o: count_binary.c count_binary.h \
+ ../cb_bsp//HAL/inc/alt_types.h \
+ ../cb_bsp//drivers/inc/altera_avalon_pio_regs.h ../cb_bsp//HAL/inc/io.h \
+ ../cb_bsp//HAL/inc/alt_types.h ../cb_bsp//HAL/inc/sys/alt_irq.h \
+ ../cb_bsp//HAL/inc/nios2.h ../cb_bsp/system.h ../cb_bsp/linker.h
+
+count_binary.h:
+
+../cb_bsp//HAL/inc/alt_types.h:
+
+../cb_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../cb_bsp//HAL/inc/io.h:
+
+../cb_bsp//HAL/inc/alt_types.h:
+
+../cb_bsp//HAL/inc/sys/alt_irq.h:
+
+../cb_bsp//HAL/inc/nios2.h:
+
+../cb_bsp/system.h:
+
+../cb_bsp/linker.h:
