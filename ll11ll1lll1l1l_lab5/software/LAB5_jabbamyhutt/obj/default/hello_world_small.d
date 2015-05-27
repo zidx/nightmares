8f73a4b4e0916f7bf1ba585422b79ab99972878a
unknown <klholden@EE-137PC02.ee.washington.edu>
2015-05-26 20:46:32 -0700

    metadata

---
+++ ll11ll1lll1l1l_lab5/software/LAB5_jabbamyhutt/obj/default/hello_world_small.d
@@ -0,0 +1,13 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../LAB5_jabbamyhutt_bsp//HAL/inc/sys/alt_stdio.h \
+ ../LAB5_jabbamyhutt_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../LAB5_jabbamyhutt_bsp//HAL/inc/io.h \
+ ../LAB5_jabbamyhutt_bsp//HAL/inc/alt_types.h
+
+../LAB5_jabbamyhutt_bsp//HAL/inc/sys/alt_stdio.h:
+
+../LAB5_jabbamyhutt_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../LAB5_jabbamyhutt_bsp//HAL/inc/io.h:
+
+../LAB5_jabbamyhutt_bsp//HAL/inc/alt_types.h:
