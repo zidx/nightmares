a5be08c08bfda332448111b5db4df33043f824a0
Zach Nehrenberg <zidx@users.noreply.github.com>
2015-05-31 17:37:34 -0700

    Create hang_politically_correct_equally_man_or_women.c

---
+++ hang_politically_correct_equally_man_or_women.c
@@ -0,0 +1,356 @@
+/* 
+ * "Small Hello World" example. 
+ * 
+ * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
+ * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example 
+ * designs. It requires a STDOUT  device in your system's hardware. 
+ *
+ * The purpose of this example is to demonstrate the smallest possible Hello 
+ * World application, using the Nios II HAL library.  The memory footprint
+ * of this hosted application is ~332 bytes by default using the standard 
+ * reference design.  For a more fully featured Hello World application
+ * example, see the example titled "Hello World".
+ *
+ * The memory footprint of this example has been reduced by making the
+ * following changes to the normal "Hello World" example.
+ * Check in the Nios II Software Developers Manual for a more complete 
+ * description.
+ * 
+ * In the SW Application project (small_hello_world):
+ *
+ *  - In the C/C++ Build page
+ * 
+ *    - Set the Optimization Level to -Os
+ * 
+ * In System Library project (small_hello_world_syslib):
+ *  - In the C/C++ Build page
+ * 
+ *    - Set the Optimization Level to -Os
+ * 
+ *    - Define the preprocessor option ALT_NO_INSTRUCTION_EMULATION 
+ *      This removes software exception handling, which means that you cannot 
+ *      run code compiled for Nios II cpu with a hardware multiplier on a core 
+ *      without a the multiply unit. Check the Nios II Software Developers 
+ *      Manual for more details.
+ *
+ *  - In the System Library page:
+ *    - Set Periodic system timer and Timestamp timer to none
+ *      This prevents the automatic inclusion of the timer driver.
+ *
+ *    - Set Max file descriptors to 4
+ *      This reduces the size of the file handle pool.
+ *
+ *    - Check Main function does not exit
+ *    - Uncheck Clean exit (flush buffers)
+ *      This removes the unneeded call to exit when main returns, since it
+ *      won't.
+ *
+ *    - Check Don't use C++
+ *      This builds without the C++ support code.
+ *
+ *    - Check Small C library
+ *      This uses a reduced functionality C library, which lacks  
+ *      support for buffering, file IO, floating point and getch(), etc. 
+ *      Check the Nios II Software Developers Manual for a complete list.
+ *
+ *    - Check Reduced device drivers
+ *      This uses reduced functionality drivers if they're available. For the
+ *      standard design this means you get polled UART and JTAG UART drivers,
+ *      no support for the LCD driver and you lose the ability to program 
+ *      CFI compliant flash devices.
+ *
+ *    - Check Access device drivers directly
+ *      This bypasses the device file system to access device drivers directly.
+ *      This eliminates the space required for the device file system services.
+ *      It also provides a HAL version of libc services that access the drivers
+ *      directly, further reducing space. Only a limited number of libc
+ *      functions are available in this configuration.
+ *
+ *    - Use ALT versions of stdio routines:
+ *
+ *           Function                  Description
+ *        ===============  =====================================
+ *        alt_printf       Only supports %s, %x, and %c ( < 1 Kbyte)
+ *        alt_putstr       Smaller overhead than puts with direct drivers
+ *                         Note this function doesn't add a newline.
+ *        alt_putchar      Smaller overhead than putchar with direct drivers
+ *        alt_getchar      Smaller overhead than getchar with direct drivers
+ *
+ */
+
+#include "sys/alt_stdio.h"
+#include "altera_avalon_pio_regs.h"
+#include "alt_types.h"
+
+#define rtd (volatile char *) 	 	 0x0003060
+#define outSignal (volatile char *)  0x0003050
+#define curByteOut (volatile char *) 0x0003040
+#define inStrobe (volatile char *)   0x0003030
+#define load (volatile char *) 		 0x0003020
+#define curByteIn (volatile char *)  0x0003010
+#define hempTea (volatile char *)    0x0003000
+
+#define MAX_GUESSES 10  // Corresponds to correct and incorrect guesses
+#define WIN_VAL 254
+#define LOSE_VAL 253
+#define DATA_RECIEVED 255
+
+// For setWord. This makes this easier
+char word[20];
+alt_u8 letterCount;
+
+
+void sendData(alt_u8 data);
+alt_u8 recieveData(void);
+void game_master();
+void player();
+void setWord();
+alt_u8 startGame(alt_u8 wordLen);
+
+
+
+int main() {
+	// Must reset everything, even the things we're not using
+	IOWR_ALTERA_AVALON_PIO_DATA(outSignal, 0x0);
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 0x0);
+	alt_u8 curByteInVal = IORD_ALTERA_AVALON_PIO_DATA(curByteIn);
+	alt_u8 hempTeaVal = IORD_ALTERA_AVALON_PIO_DATA(hempTea);
+	alt_u8 inStrobeVal = IORD_ALTERA_AVALON_PIO_DATA(inStrobe);
+	IOWR_ALTERA_AVALON_PIO_DATA(outSignal, 0x0);
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 0x0);
+	players = 0;
+
+	alt_printf("\nInitial prevVal %c \n", curByteInVal);
+
+	char start = 'a';
+	while(start != 'M' && start != 'P') {
+		alt_putstr("Enter an 'M' to be game master or 'P' to be a player");
+		start = alt_getchar();
+	}
+	
+	if (start == 'M')
+		game_master();
+	else
+		player();
+
+}
+
+void game_master() {
+	while (1) {
+		// Set word
+		alt_getchar();  // Recieve dummy byte
+		alt_u8 wordLen = setWord();
+
+		// Word placements
+		int letterPlacement[wordLen];
+
+		// Now we recieve input from the player and check it
+		int guesses = 0;
+		int win = 0;
+		alt_u8 guessedLetters[MAX_GUESSES];  // Already guessed letters
+
+		// Send to the player to say that we're ready for guesses
+		// We use 255 for send
+		// Wait for the return signal from the player so we know to start the game
+		// If we don't get this, something has gone wrong and we need to retry
+		alt_u8 start = startGame(wordLen);
+
+		if (!start)
+			alt_putstr("Something has gone horribly wrong\n");
+
+		// Now we are ready to wait to recieve data
+		alt_u8 guess;
+		alt_u8 retVal;
+		while (!win || guesses < MAX_GUESSES) {
+			guess = recieveData();
+			int correct = 0;
+			int i;
+			retVal = 0; // 1 - wordLen == correct, 0 == wrong, wordLen+1 = alreadyGuessed
+
+			// Check if the letter is already guessed
+			for (i = 0; i < guesses; i++) {
+				if (guessedLetters[i] == guess) {
+					alt_putstr("This letter has already been guessed!\n");
+					retVal = wordLen+1;
+				}
+			}
+			if (retVal == 0) {
+				guessedLetters[guesses] = guess;
+				for (i = 0; i < wordLen; i++) {
+					if (word[i] = guess) {
+						retVal++;
+						letterPlacement[i] = 1;
+					}
+				}
+				// Increment guess value
+				guesses++;
+			}
+
+			// Display letter placements
+			int lettersCorrect = 0;
+			alt_putstr("Word: ");
+			for (i = 0; i < wordLen; i++) {
+				if (letterPlacement[i] == 1) {
+					alt_printf("%c ", word[i]);
+					lettersCorrect++;
+				} else
+					alt_putstr("_ ");
+			}
+			alt_putstr("\nGuessed Letters: ");
+			for (i = 0; i < guesses; i++) {
+				if (i == 0)
+					alt_printf("%c", guessedLetters[i]);
+				else
+					alt_printf(", %c", guessedLetters[i]);
+			}
+			alt_putstr("\n");
+
+			// Send back the guessed answer
+			sendData(retVal);
+			recieveData();
+			// Send back how many guesses remain
+			sendData(MAX_GUESSES - guesses);
+		}
+
+		// Check win condition
+		if (lettersCorrect == wordLen) {
+			sendData(WIN_VAL);
+			alt_getchar();
+			alt_putstr("Player won!\nPlay again?");
+			char response = alt_getchar();
+			if (response != 'y') {
+				break;
+			}
+		}
+		else {
+			sendData(LOSE_VAL);
+			alt_getchar();
+			alt_putstr("Player Lost\nPlay again?");
+			char response = alt_getchar();
+			if (response != 'y') {
+				break;
+			}
+		}
+	}
+}
+
+void player() {
+	while (1) {
+		// Wait for the code that the master is ready
+		int wordLen = recieveData();
+		alt_u8 guessesRemaining = MAX_GUESSES;
+		int win = 0;
+		// Send that we got the word and are about to play
+		sendData(DATA_RECIEVED);
+		
+		while (1) {
+			// Now we are ready to send guesses!
+			alt_getchar();
+			alt_putstr("Please enter a letter and press enter\n");
+			alt_u8 letter = alt_getchar();
+			
+			// Send the letter
+			sendData(letter);
+			
+			// Get the response
+			alt_u8 response = recieveData();
+			
+			if (response == WIN_VAL) {
+				alt_putstr("Congratulations you won!\nWaiting for new game...\n");
+				break;
+			}
+			else if (response == LOSE_VAL) {
+				alt_putstr("You lost because you have no friends and your life is\nmeaningless and you smell bad.\nWaiting for new game...\n");
+				break;
+			}
+			// If we come here than we're in normal gameplay
+			sendData(DATA_RECIEVED);
+			guessesRemaining = recieveData();
+			
+			if (response == 0) {
+				alt_printf("Sorry, the letter '%c' was incorrect\n", letter);
+			}
+			if (response <= wordLen) {
+				alt_printf("The letter '%c' was correct and filled %x places!\n", letter, response);
+			}
+			else 
+				alt_printf("You already guessed the letter '%c'!\n", letter);
+			
+			alt_printf("You now have '%x' guesses remaining.\n", guessesRemaining);
+		}
+	}
+}
+
+void setWord() {
+	alt_putstr("Please enter your word (Max 20 letters):\n");
+	char letter = '#';
+	letterCount = 0;
+	while ((letter = alt_getchar()) != EOF) {
+		word[letterCount] = letter;
+		letterCount++;
+		if (letterCount == 20) {
+			alt_putstr("Max word length reached.\n");
+		}
+	}
+	if (letterCount == 0) {
+		alt_putstr("You must enter at least one character. Please try again\n");
+		setWord();
+	}
+}
+
+// Retries the send/recieve pattern after a timeout
+// until data is recieved.
+alt_u8 startGame(alt_u8 wordLen) {
+	alt_u8 retVal = 0;
+	int wait = 0;
+	int timeout = 0;
+	sendData(wordLen);
+	alt_u8 strobe = IORD_ALTERA_AVALON_PIO_DATA(inStrobe);
+	do {
+		while (strobe == 0) {
+			strobe = IORD_ALTERA_AVALON_PIO_DATA(inStrobe);
+			wait++;
+			if (wait == 200000) {
+				alt_putstr("Player waiting timeout. Retrying...\n");
+				timeout = 1;
+				break;
+			}
+		}
+		if (strobe == 1) {
+			retVal = IORD_ALTERA_AVALON_PIO_DATA(curByteIn);
+			if (retVal == DATA_RECIEVED)  // Value to show data was received
+				return 1;
+			else {
+				alt_putstr("Incorrect signal recieved. Retrying...");
+				timeout = 1;
+			}
+			timeout = 0;
+		}
+		wait = 0;
+	} while (timeout == 1);
+
+	// Should never get here
+	return 0;
+}
+
+alt_u8 recieveData() {
+	alt_u8 strobe = IORD_ALTERA_AVALON_PIO_DATA(inStrobe);
+	while (strobe == 0) {
+		strobe = IORD_ALTERA_AVALON_PIO_DATA(inStrobe);
+	}
+	return IORD_ALTERA_AVALON_PIO_DATA(curByteIn);
+}
+
+
+void sendData(alt_u8 data) {
+	alt_u8 empty = 0;
+	//sends data to camera
+	IOWR_ALTERA_AVALON_PIO_DATA(curByteOut, data);
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 0x1);
+
+	empty = IORD_ALTERA_AVALON_PIO_DATA(hempTea);
+	while (empty == 0) {
+		empty = IORD_ALTERA_AVALON_PIO_DATA(hempTea);
+	}
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 0x0);
+}
